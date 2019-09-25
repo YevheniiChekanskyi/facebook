155190f334a62898e8ffad75e10a5e8e18cd424b
Yevhenii Chekanskyi <echekanskiy@cybervisiontech.com>
2019-09-25 14:25:11 +0300

    CDAP-15860 Facebook Ads (SaaS source) - streaming source.

---
+++ docs/FacebookStreamingSource-streamingsource.md
@@ -0,0 +1,31 @@
+# Facebook ADS batch source
+
+Description
+-----------
+This plugin used to query Facebook Insights API, periodically fetching fresh data.
+
+Properties
+----------
+### General
+
+**Reference Name:** Name used to uniquely identify this source for lineage, annotating metadata, etc.
+
+**Access token:** Access token to be used to authenticate in Facebook API.
+### Object query parameters
+
+**Object Id:** Object identifier in Facebook API.
+
+**Object Type:** Object type that represented by **Object Id**.
+
+**Level:** Query level
+
+**Fields:** Fields to be queried.
+
+**Breakdowns:** Breakdowns to be applied to query.
+
+**Sorting:** Sorting rules to be applied to query.
+### API limits
+
+**Filtering:** Filers that will specify rules to select objects.
+
+**Time ranges:** Time ranges to be applied to query.
---
+++ src/main/java/io/cdap/plugin/facebook/source/streaming/FacebookStreamingSource.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright © 2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.plugin.facebook.source.streaming;
+
+import io.cdap.cdap.api.annotation.Description;
+import io.cdap.cdap.api.annotation.Name;
+import io.cdap.cdap.api.annotation.Plugin;
+import io.cdap.cdap.api.data.format.StructuredRecord;
+import io.cdap.cdap.api.dataset.DatasetProperties;
+import io.cdap.cdap.etl.api.FailureCollector;
+import io.cdap.cdap.etl.api.PipelineConfigurer;
+import io.cdap.cdap.etl.api.streaming.StreamingContext;
+import io.cdap.cdap.etl.api.streaming.StreamingSource;
+import io.cdap.plugin.common.Constants;
+import io.cdap.plugin.common.IdUtils;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import scala.reflect.ClassTag;
+import scala.reflect.ClassTag$;
+
+/**
+ * Plugin reads data from Facebook Insights Api periodically fetching fresh data.
+ */
+@Plugin(type = StreamingSource.PLUGIN_TYPE)
+@Name(FacebookStreamingSource.NAME)
+@Description(FacebookStreamingSource.DESCRIPTION)
+public class FacebookStreamingSource extends StreamingSource<StructuredRecord> {
+  static final String NAME = "FacebookStreamingSource";
+  static final String DESCRIPTION = "Read data from Facebook Insights API periodically fetching fresh data.";
+  private FacebookStreamingSourceConfig config;
+
+  public FacebookStreamingSource(FacebookStreamingSourceConfig config) {
+    this.config = config;
+  }
+
+  @Override
+  public void configurePipeline(PipelineConfigurer pipelineConfigurer) {
+    IdUtils.validateId(config.referenceName);
+    pipelineConfigurer.createDataset(config.referenceName, Constants.EXTERNAL_DATASET_TYPE, DatasetProperties.EMPTY);
+    validateConfiguration(pipelineConfigurer.getStageConfigurer().getFailureCollector());
+  }
+
+  @Override
+  public JavaDStream<StructuredRecord> getStream(StreamingContext context) {
+    validateConfiguration(context.getFailureCollector());
+
+    ClassTag<StructuredRecord> tag = ClassTag$.MODULE$.apply(StructuredRecord.class);
+    FacebookInputDStream dstream = new FacebookInputDStream(context.getSparkStreamingContext().ssc(), tag, config);
+    return JavaDStream.fromDStream(dstream, tag);
+  }
+
+  private void validateConfiguration(FailureCollector failureCollector) {
+    config.validate(failureCollector);
+    failureCollector.getOrThrowException();
+  }
+}
